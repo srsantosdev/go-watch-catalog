90f098155112cb20d2ec6014691bef857c836d24
Samuel Ramos <samuelsantos7@yahoo.com.br>
2020-05-31 00:31:50 -0300

    Visualização de uma lista

--- mobile/src/pages/List/index.js
+++ mobile/src/pages/List/index.js
@@ -1,16 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import { FlatList } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 import Options from "./../../components/Options";
-import { Container, Header, BackButton, TitlePage } from "./styles";
+import {
+  Container,
+  Header,
+  BackButton,
+  TitlePage,
+  Title,
+  Box,
+  Item,
+  Duration,
+  FavoriteContainer,
+  DurationTitle,
+  DurationValue,
+  EmptyListContainer,
+  TitleEmpty,
+  Message,
+  Illustration,
+} from "./styles";
+
+import illustration from "./../../assets/RoboListaVazia.png";
+
+const Favorite = () => {
+  const [active, setActive] = useState();
+
+  return (
+    <FavoriteContainer onPress={() => setActive(!active)}>
+      <Ionicons
+        name={active ? "ios-heart" : "ios-heart-empty"}
+        size={30}
+        color="#29a19c"
+      />
+    </FavoriteContainer>
+  );
+};
+
+const EmptyList = () => {
+  return (
+    <EmptyListContainer>
+      <Illustration source={illustration} />
+      <TitleEmpty>Ah, não!</TitleEmpty>
+      <Message>Nada foi adicionado aqui.</Message>
+    </EmptyListContainer>
+  );
+};
 
 const List = () => {
   const navigation = useNavigation();
-
   const options = [
     { name: "Editar Lista", task: () => {} },
     { name: "Excluir Lista", task: () => {} },
   ];
+
+  const [data, setData] = useState([]);
 
   return (
     <Container>
@@ -21,6 +65,27 @@ const List = () => {
         <TitlePage>Edredom</TitlePage>
         <Options options={options} />
       </Header>
+
+      {data.length > 0 ? (
+        <FlatList
+          data={data}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <Item>
+              <Box>
+                <Title>Titulo do filme</Title>
+                <Duration>
+                  <DurationTitle>Duração: </DurationTitle>
+                  <DurationValue>1h 33m</DurationValue>
+                </Duration>
+              </Box>
+              <Favorite />
+            </Item>
+          )}
+        />
+      ) : (
+        <EmptyList />
+      )}
     </Container>
   );
 };
--- mobile/src/pages/List/styles.js
+++ mobile/src/pages/List/styles.js
@@ -23,3 +23,70 @@ export const TitlePage = styled.Text`
   color: #ffffff;
   text-align: center;
 `;
+
+export const FavoriteContainer = styled.TouchableOpacity``;
+
+export const Item = styled.TouchableOpacity`
+  height: 55px;
+  background: #121214;
+  border-radius: 5px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 5px;
+  padding: 12px 20px;
+`;
+
+export const Box = styled.View``;
+
+export const Title = styled.Text`
+  font-family: "Roboto-Bold";
+  font-size: 14px;
+  text-transform: uppercase;
+  color: #29a19c;
+`;
+
+export const Duration = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const DurationTitle = styled.Text`
+  font-family: "Roboto-Regular";
+  font-size: 12px;
+  color: #15514e;
+`;
+
+export const DurationValue = styled.Text`
+  font-family: "Roboto-Medium";
+  font-size: 12px;
+  color: #ffffff;
+`;
+
+export const EmptyListContainer = styled.View`
+  position: absolute;
+  top: 30px;
+  left: 20px;
+  height: 100%;
+  width: 100%;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
+export const Illustration = styled.Image``
+
+export const TitleEmpty = styled.Text`
+  font-family: "Roboto-Medium";
+  font-size: 18px;
+  color: #fff;
+  margin-top: 32px;
+`
+
+export const Message = styled.Text`
+  font-family: "Roboto-Light";
+  font-size: 14px;
+  line-height: 16px;
+  letter-spacing: 1.5px;
+  color: #29a19c;
+`
